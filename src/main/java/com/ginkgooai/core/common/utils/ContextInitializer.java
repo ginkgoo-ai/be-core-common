f82fa7ec47179a248d72eea8091b5addb04d893e
Wang <645565009@qq.com>
2025-03-04 11:44:16 +0800

    add jwt contexts

--- src/main/java/com/ginkgooai/core/common/utils/ContextInitializer.java
+++ src/main/java/com/ginkgooai/core/common/utils/ContextInitializer.java
@@ -1,20 +1,28 @@
 package com.ginkgooai.core.common.utils;
 
 import com.ginkgooai.core.common.constant.ContextsConstant;
-import com.nimbusds.jose.util.StandardCharset;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
-import javax.crypto.SecretKey;
+import java.util.Collection;
 import java.util.Enumeration;
 
 @Slf4j
 public class ContextInitializer implements ContextsConstant {
 
     private static final String HEADER_WORKSPACE_ID = "x-workspace-id";
+
+    private static final String PREFIX = "t-";
+
+
+    public static void load(HttpServletRequest request) {
+        loadFromHeaders(request);
+        loadFromSecurityContext();
+    }
+
 
     public static void loadFromHeaders(HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaderNames();
@@ -23,31 +31,25 @@ public class ContextInitializer implements ContextsConstant {
             if (header.startsWith(HEADER_WORKSPACE_ID)) {// 去掉前缀
                 ContextUtils.set(ContextsConstant.WORKSPACE_ID, request.getHeader(header));
             }
+
+            if (header.startsWith(PREFIX)) {
+                ContextUtils.set(header, request.getHeader(header));
+            }
         }
     }
 
-    public static void loadFromJWT(HttpServletRequest request, String secretKey) {
-        String authorizationHeader = request.getHeader("Authorization");
-
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+    public static void loadFromSecurityContext() {
+        if(SecurityContextHolder.getContext() == null || SecurityContextHolder.getContext().getAuthentication() == null) {
             return;
         }
 
-        String token = authorizationHeader.substring(7);
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        Claims claims = verifyToken(token, secretKey);
-
-        request.setAttribute("userId", claims.getSubject());
+        ContextUtils.set(ContextsConstant.USER_ID, jwt.getClaimAsString("sub"));
+        ContextUtils.set(ContextsConstant.USER_EMAIL, jwt.getClaimAsString("email"));
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        ContextUtils.set(ContextsConstant.USER_ROLE, authorities);
     }
 
-    public static Claims verifyToken(String token, String secretKey) {
 
-        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharset.UTF_8));
-
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
 }
