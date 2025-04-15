b8d7d34fdb481d402ae953057d21ef78d62bda49
seasonsolt <seasonsolt@gmail.com>
2025-04-15 15:54:59 +0800

    feat: add description field to ActivityType enum

--- src/main/java/com/ginkgooai/core/common/enums/ActivityType.java
+++ src/main/java/com/ginkgooai/core/common/enums/ActivityType.java
@@ -9,62 +9,67 @@ import java.util.List;
 public enum ActivityType {
 
 	PROJECT_CREATED(
-			"{user} created project '{project}'.",
+			"created a new project", "{user} created project '{project}'.",
 		Arrays.asList("user", "project"),
 		true,
 		null
 	),
 	PROJECT_STATUS_CHANGE(
-			"'{project}' status changed from {previousStatus} to {newStatus}.",
+			"changed project status", "'{project}' status changed from {previousStatus} to {newStatus}.",
 		Arrays.asList("project", "previousStatus", "newStatus"),
 		true,
 		null
 	),
 	ROLE_CREATED(
-			"{user} created '{roleName}' in '{project}'.",
+			"role created", "{user} created '{roleName}' in '{project}'.",
 		Arrays.asList("user", "roleName", "project"),
 		true,
 		null
 	),
 	ROLE_STATUS_UPDATE(
-			"'{roleName}' status updated to {newStatus}.",
+			"updated role status", "'{roleName}' status updated to {newStatus}.",
 		Arrays.asList("roleName", "newStatus"),
 		true,
 		null
 	),
-	TALENT_IMPORTED("Talent {talentName} is imported.", Arrays.asList("talentName"),
+	TALENT_IMPORTED("imported a talent", "Talent {talentName} is imported.", Arrays.asList("talentName"),
 		true,
 			"spreadsheet"),
 	SUBMISSION_ADDED(
-			"New submission added in {talentName}'s application for \"{roleName}\" of {project}.",
+			"added a submission", "New submission added in {talentName}'s application for \"{roleName}\" of {project}.",
 			Arrays.asList("talentName", "roleName", "project"),
 		true,
 		"video"
 	),
 	SUBMISSION_ADDED_TO_SHORTLIST(
-			"{talentName}'s for \"{roleName}\" of \"{project}\" has benn shortlisted.",
+			"added a submission to shortlist",
+			"{talentName}'s for \"{roleName}\" of \"{project}\" has been shortlisted.",
 		Arrays.asList("user", "talentName", "videoName"),
 		true,
 		"video"
 	),
 	TALENT_DIRECT_UPLOAD(
-			"{talentName} uploaded a submission for {roleName} of {project}.",
+			"uploaded a submission", "{talentName} uploaded a submission for {roleName} of {project}.",
 			Arrays.asList("talentName", "roleName", "project"),
 		true,
 		"video"
 	),
-	FEEDBACK_ADDED("New comment added in {talentName}’s application for \"{roleName}\" of \"{project}\".",
+	FEEDBACK_ADDED("feedback added",
+			"New comment added in {talentName}'s application for \"{roleName}\" of \"{project}\".",
 			Arrays.asList("talentName", "roleName", "project"),
 		true,
 		"text"
 	);
 
+	private final String description;
 	private final String template;
 	private final List<String> variables;
 	private final boolean projectRelated;
 	private final String attachmentType;
 
-	ActivityType(String template, List<String> variables, boolean projectRelated, String attachmentType) {
+	ActivityType(String description, String template, List<String> variables, boolean projectRelated,
+			String attachmentType) {
+		this.description = description;
 		this.template = template;
 		this.variables = variables;
 		this.projectRelated = projectRelated;
